9aa4db702b94b348fadffa82f96ab48a11f5c86c
FresHHerB <faelzim34@gmail.com>
2025-09-05 18:19:24 -0300

    Updated SettingsPage.tsx

--- src/components/SettingsPage.tsx
+++ src/components/SettingsPage.tsx
@@ -852,3 +852,6 @@ const SettingsPage: React.FC<SettingsPageProps> = ({ user, onBack }) => {
                     type="button"
                     onClick={resetVoiceForm}
                     className="px-6 py-2 text-gray-400 hover:text-white hover:bg-gray-800 rounded-lg transition-all duration-200"
+                  >
+                    Cancelar
+                  </button>
