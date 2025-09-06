e7d366b2099b5354c070ee1cfa06261707db36b6
FresHHerB <faelzim34@gmail.com>
2025-09-06 04:10:13 -0300

    Updated ScriptGenerationPage.tsx

--- src/components/ScriptGenerationPage.tsx
+++ src/components/ScriptGenerationPage.tsx
@@ -279,7 +279,7 @@ const ScriptGenerationPage: React.FC<ScriptGenerationPageProps> = ({ user, onBac
 
       console.log('📤 Payload enviado:', payload);
 
-      const response = await fetch('https://n8n-n8n.h5wo9n.easypanel.host/webhook/gerarRoteiro', {
+      const response = await fetch('https://n8n-n8n.h5wo9n.easypanel.host/webhook/gerarRoteiro2', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
