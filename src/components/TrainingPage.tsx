40d08848af86ce48ba048f757ee4cdd74d1bf102
FresHHerB <faelzim34@gmail.com>
2025-09-07 18:21:40 -0300

    Increase modal width by 15% on each side

--- src/components/TrainingPage.tsx
+++ src/components/TrainingPage.tsx
@@ -433,6 +433,7 @@ const TrainingPage: React.FC<TrainingPageProps> = ({ user, onBack, onNavigate })
         >
           <div 
             className="bg-gray-900 rounded-2xl border border-gray-700 w-full max-w-4xl h-[95vh] overflow-hidden flex flex-col"
+            className="bg-gray-900 rounded-2xl border border-gray-700 w-full max-w-6xl h-[95vh] overflow-hidden flex flex-col"
             onClick={(e) => e.stopPropagation()}
           >
             {/* Modal Header */}
